db8675baccb9f1613db7773cda6af629bc6dcbde
Ramesh Raj <draj56403@gmail.com>
2026-05-28 20:59:46 +0530

    Implemented end-to-end Selenium automation framework

--- tests/test_user_management.py
+++ tests/test_user_management.py
@@ -13,8 +13,8 @@ def test_search_user(setup):
 
     logging_page.login("Admin", "admin123")
     dash_board.click_admin_tab()
-    user_login_page.search_user("test_user@10")
+    user_login_page.search_user("test_user@13")
     user_login_page.user_page()
-    user_login_page.username_key("Peter Mac Anderson")
+    user_login_page.username_key("Chris John Joe")
     user_login_page.user_search_act()
-    assert user_login_page.is_user_present("test_user@10")
+    assert user_login_page.is_user_present("test_user@13")
